0acab85585e0f73e5de108d2e91b1d8833fe86a5
Axel Jeunet--Bailly <axel.jeunet--bailly@grenoble-inp.org>
2025-03-03 18:32:59 +0100

    (fix) Update queries and lutations in frontend

--- AGPS_/AGPS/frontend/src/utils/mutations.js
+++ AGPS_/AGPS/frontend/src/utils/mutations.js
@@ -1,11 +1,11 @@
-function useRebalanceMutation( { datetime } ) {
+function useRebalanceMutation( { date } ) {
     return useMutation(
         {
             mutationFn: async () => {
                 const res = await fetch(getAPIUrl("rebalance"), {
                       method: 'POST',
                       headers: { 'Content-Type': 'application/json' },
-                      body: JSON.stringify({ datetime })
+                      body: JSON.stringify({ date: date.toISOString() })
                 });
 
                 const resJson = await res.json();
@@ -17,8 +17,12 @@ function useRebalanceMutation( { datetime } ) {
                 return resJson;
             },
             onSuccess: () => {
-                QUERY_CLIENT.invalidateQueries({ queryKey: ['information', { datetime }] })
-                QUERY_CLIENT.invalidateQueries({ queryKey: ['rebalancingInformation', { datetime }] })
+                QUERY_CLIENT.invalidateQueries({ queryKey: ['information', { date }] })
+                QUERY_CLIENT.invalidateQueries({ queryKey: ['rebalancingInformation', { date }] })
             }
         });
+}
+
+export {
+    useRebalanceMutation
 }
--- AGPS_/AGPS/frontend/src/utils/queries.js
+++ AGPS_/AGPS/frontend/src/utils/queries.js
@@ -3,14 +3,14 @@ import getAPIUrl from './url';
 
 const QUERY_CLIENT = new QueryClient();
 
-function usePortfolioInformationQuery({ datetime }) {
+function usePortfolioInformationQuery({ date }) {
     return useQuery({
-        queryKey: ['information', { datetime }],
+        queryKey: ['information', { date }],
         queryFn: async () => {
             const res = await fetch(getAPIUrl("information"), {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ datetime })
+                body: JSON.stringify({ date: date.toISOString() })
             });
 
             const resJson = await res.json();
@@ -24,14 +24,14 @@ function usePortfolioInformationQuery({ datetime }) {
     });
 }
 
-function usePortfolioRebalancingInformationQuery({ datetime }) {
+function usePortfolioRebalancingInformationQuery({ date }) {
     return useQuery({
-        queryKey: ['rebalancingInformation', { datetime }],
+        queryKey: ['rebalancingInformation', { date }],
         queryFn: async () => {
             const res = await fetch(getAPIUrl("rebalancingInformation"), {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ datetime })
+                body: JSON.stringify({ date: date.toISOString() })
             });
 
             const resJson = await res.json();
@@ -46,5 +46,7 @@ function usePortfolioRebalancingInformationQuery({ datetime }) {
 }
 
 export {
+    usePortfolioInformationQuery,
+    usePortfolioRebalancingInformationQuery,
     QUERY_CLIENT
 }
