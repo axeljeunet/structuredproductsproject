1e4d67e07a4b6d7f56ffc3c2da4d70174cd7705b
Axel Jeunet--Bailly <axel.jeunet--bailly@grenoble-inp.org>
2025-03-03 18:44:42 +0100

    (feat) Add +1 day

--- AGPS_/AGPS/frontend/src/App.tsx
+++ AGPS_/AGPS/frontend/src/App.tsx
@@ -4,21 +4,17 @@ import PortfolioCompositionSection from './components/portfolio_composition/Port
 import RebalancingSection from './components/rebalancing/RebalancingSection';
 import PortfolioGlobalInformationsSection from './components/portfolio_global/PortfolioGlobalInformationsSection';
 import Box from '@mui/material/Box';
-import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import PortfolioInformationProvider from './components/PortfolioInformationProvider';
+import DateInformation from './components/DateInformation';
 
 const INITAL_DATE = '05/07/2000';
 
 function App() {
   const [date, setDate] = useState(new Date(INITAL_DATE))
+
   return (
     <Box className="App" sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-        <DateTimePicker
-          label="Choose a day"
-          value={date}
-          views={['year', 'month', 'day']}
-          onChange={(newValue) => newValue && setDate(newValue)}
-        />
+        <DateInformation date={date} setDate={setDate} />
         <PortfolioInformationProvider date={date}>
           <PortfolioCompositionSection />
           <RebalancingSection />
---
+++ AGPS_/AGPS/frontend/src/components/DateInformation.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import { Box, Button, Typography } from '@mui/material';
+  
+export default function DateInformation({ date, setDate }) {
+
+    const incrementDate = () => {
+        const newDate = new Date(date);
+        newDate.setDate(newDate.getDate() + 1);
+        setDate(newDate);
+      };
+
+    return (
+        <Box className="DateInformation" sx={{ display: "flex", flexDirection: "row", gap: 2 }}>
+            <Button onClick={incrementDate}>
+            <Typography>DAY +1</Typography>
+            </Button>
+            <DateTimePicker
+            label="Choose a day"
+            value={date}
+            views={['year', 'month', 'day']}
+            onChange={(newValue) => newValue && setDate(newValue)}
+            />
+        </Box>
+    );
+}
